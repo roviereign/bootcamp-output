ac2a25f5aed4d6d2832aaddac68a469ee2cc1ebd
rovie.basa@gsu.eu.ph <rovie.basa@gsu.eu.ph>
2024-04-19 13:52:13 +0800

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+let menu_icon = document.querySelector("#menu_icon");
+let menu = document.querySelector(".nav_menu");
+
+menu_icon.addEventListener("click", function(){
+    menu.classList.toggle("show")
+}); 
